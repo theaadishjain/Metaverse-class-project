8092d8aafee1e99f7bb67c2144d3ef9e4d65b2f9
Aadish Jain <165540156+theaadishjain@users.noreply.github.com>
2025-02-08 10:49:18 +0530

    Update lightonoff.cs

--- lightonoff.cs
+++ lightonoff.cs
@@ -1,27 +1,31 @@
 using UnityEngine;
 
-public class lightonoff : MonoBehaviour
+public class LightOnOff : MonoBehaviour
 {
-    public int x;
-    public GameObject[] my_light;
-   
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public int x; 
+    public GameObject[] my_light; 
+    public string y; 
+
+    // Start is called once before the first frame update
     void Start()
     {
-        for(int i = 0; i < 3; i++)
+   
+        /*for (int i = 0; i < 3; i++)
         {
-            my_light[i].SetActive(false);
-        }
-       /* my_light[0].SetActive(false);
-        my_light[1].SetActive(false);
-        my_light[2].SetActive(false);*/
-
-
+            my_light[i].SetActive(false); 
+        }*/
     }
 
-    // Update is called once per frame
     void Update()
     {
+       
+        for (int i = 0; i < my_light.Length; i++)
+        {
         
+            if (my_light[i].name == y)
+            {
+                my_light[i].SetActive(false); 
+            }
+        }
     }
 }
